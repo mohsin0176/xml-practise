337819666f668b1775f1943a075629b7f8c5343f
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-28 10:06:32 -0700

    parse xml to json

---
+++ node xml parser/xml_parser.js
@@ -0,0 +1,15 @@
+const fs=require("fs");
+const xml2js=require("xml2js");
+const util=require("util");
+
+const parser=new xml2js.Parser();
+
+fs.readFile("example.xml",(err,data)=>{
+
+parser.parseString(data,(err,result)=>{
+
+ console.log(util.inspect(result,false,null,true));
+
+});
+
+});
